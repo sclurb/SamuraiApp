4b2a4053f4c774eb5e76b1785d76515446d8c64f
sclurb <sclurb@outlook.com>
2020-09-14 10:34:02 -0400

    Completed Module-2

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -9,5 +9,12 @@ namespace SamuraiApp.Data
     public class SamuraiContext : DbContext
     {
         public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData;");
+        }
     }
 }
